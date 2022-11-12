1608c938b3ff3c6aa2065c07576dfadd71ed0988
Jason Power <20076537@mail.wit.ie>
2022-11-12 23:12:38 +0000

    Added a rough song info activity

--- app/src/main/java/org/wit/playlistmanager/activities/SongListActivity.kt
+++ app/src/main/java/org/wit/playlistmanager/activities/SongListActivity.kt
@@ -1,15 +1,19 @@
 package org.wit.playlistmanager.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.wit.playlistmanager.adapters.SongAdapter
+import org.wit.playlistmanager.adapters.SongListener
 import org.wit.playlistmanager.databinding.ActivitySongListBinding
 import org.wit.playlistmanager.main.MainApp
 import org.wit.playlistmanager.models.playlist.PlaylistModel
 import org.wit.playlistmanager.models.song.SongModel
 
-class SongListActivity : AppCompatActivity() {
+class SongListActivity : AppCompatActivity(), SongListener {
     lateinit var app: MainApp
     private lateinit var binding: ActivitySongListBinding
 
@@ -28,9 +32,24 @@ class SongListActivity : AppCompatActivity() {
         }
 
         playlist.songs.removeAt(0)
-
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = SongAdapter(playlist.songs,this)
     }
+
+    override fun onSongPressed(song: SongModel) {
+        val launcherIntent = Intent(this, SongInfoActivity::class.java)
+        launcherIntent.putExtra("song_info", song)
+        getClickResult.launch(launcherIntent)
+    }
+
+    private val getClickResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                (binding.recyclerView.adapter)?.
+                notifyItemRangeChanged(0,app.playlists.findAll().size)
+            }
+        }
 }
