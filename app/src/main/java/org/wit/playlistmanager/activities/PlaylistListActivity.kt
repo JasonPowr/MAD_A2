921e13c5498ee7b0db07b75b1a4d3536c01690ab
Jason Power <20076537@mail.wit.ie>
2022-12-01 17:45:18 +0000

    Added filter for playlists

--- app/src/main/java/org/wit/playlistmanager/activities/PlaylistListActivity.kt
+++ app/src/main/java/org/wit/playlistmanager/activities/PlaylistListActivity.kt
@@ -1,16 +1,20 @@
 package org.wit.playlistmanager.activities
 
-import org.wit.playlistmanager.main.MainApp
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
+import org.wit.playlistmanager.R
 import org.wit.playlistmanager.adapters.PlaylistAdapter
 import org.wit.playlistmanager.adapters.PlaylistListener
 import org.wit.playlistmanager.databinding.ActivityPlaylistListBinding
+import org.wit.playlistmanager.main.MainApp
 import org.wit.playlistmanager.models.playlist.PlaylistModel
+
 
 class PlaylistListActivity : AppCompatActivity(), PlaylistListener {
     private lateinit var binding: ActivityPlaylistListBinding
@@ -33,6 +37,30 @@ class PlaylistListActivity : AppCompatActivity(), PlaylistListener {
             val launcherIntent = Intent(this, PlaylistActivity::class.java)
             getClickResult.launch(launcherIntent)
         }
+    }
+
+    fun filter(filteredNames: List<PlaylistModel>){
+        binding.recyclerViewplaylist.adapter = PlaylistAdapter(filteredNames,this)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        val menuSearchItem = menu.findItem(R.id.search)
+        val searchView = menuSearchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                filter(app.playlists.filterPlaylistNames(query))
+                return true
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onEditButtonClick(playlist: PlaylistModel) {
--- app/src/main/java/org/wit/playlistmanager/models/playlist/PlaylistStore.kt
+++ app/src/main/java/org/wit/playlistmanager/models/playlist/PlaylistStore.kt
@@ -8,5 +8,7 @@ interface PlaylistStore {
     fun update(playlist: PlaylistModel)
     fun addSongToPlaylist(playlist: PlaylistModel, song: SongModel)
     fun updateSongInPlaylist(playlist: PlaylistModel, song: SongModel)
+    fun findAllPlaylistNames(): ArrayList<String>
     fun findAllSongs(playlist: PlaylistModel): List<SongModel>
+    fun filterPlaylistNames(playListName: String): List<PlaylistModel>
 }
